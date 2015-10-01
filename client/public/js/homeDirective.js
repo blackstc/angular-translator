bbc6137c15bcac52dd679ae53b5f8e17fc1f7093
Blackstock <charlieblackstock@gmail.com>
2015-10-01 09:17:06 -0600

    updated home page into directives

---
+++ client/public/js/homeDirective.js
@@ -0,0 +1,13 @@
+myApp.directive('ngSignIn', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '../../views/signIn.html'
+  };
+});
+
+myApp.directive('ngUserTable', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '../../views/userTable.html'
+  };
+});
